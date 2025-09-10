d593bbb846a32ceaa59ce6d8ce497787759436a6
duduusxz <eduardoataidemelo@gmail.com>
2025-09-10 00:04:51 -0300

    Finalizando componente de perfil lateral com React. Ainda para colocar os ícones nos botões.

--- procin-frontend/src/components/SidebarPerfil.jsx
+++ procin-frontend/src/components/SidebarPerfil.jsx
@@ -1,5 +1,6 @@
 import styles from '../style/SidebarPerfil.module.css'
 import logo from '../assets/logo_preta.png'
+import usuario from '../assets/usuario.webp'
 
 function PerfilLateral() {
   return (
@@ -8,9 +9,17 @@ function PerfilLateral() {
         <div className={styles.logo}>
             <img src={logo} alt="Logo" id='logo'/>
         </div>
-      
-      <img src="/default.png" alt="Foto de perfil" />
-      <p>Nome do Uuário</p>
+
+        <div className={styles.conteudo}>
+            <img src={usuario} alt="Foto de perfil"className={styles.imagem_usuario}/>
+            <p id={styles.nome_usuario}>Nome do Uuário</p>
+            <p id={styles.descricao_usuario}>Descrição sobre usuário. Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+            <button>Editar perfil</button>
+            <button>Mensagens</button>
+            <button>Meus serviços</button>
+            <hr />
+            <p id={styles.nota_perfil}>Nota do perfil: 4,9 </p>
+        </div>    
     </div>
     </>
   )
