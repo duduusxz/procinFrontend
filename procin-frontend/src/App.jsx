e80c071097d0db8922aeb3d4057bc3275395ce41
KevinAllmeida <kevitorafael@gmail.com>
2025-10-01 16:41:58 -0300

    ajuste tela login react

--- procin-frontend/src/App.jsx
+++ procin-frontend/src/App.jsx
@@ -2,12 +2,13 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/home.jsx";
 import Carrinho from "./pages/carrinho.jsx";
 import PagamentoPendente from "./components/pagamentoPendente.jsx";
+import Login from "./pages/login.jsx";
 
 function App() {
   return (
     <div>
       <Routes>
-        <Route path="/" element={<Carrinho />} />
+        <Route path="/" element={<Login />} />
       </Routes>
     </div>
   );
---
+++ procin-frontend/src/pages/login.jsx
@@ -0,0 +1,77 @@
+import React, { useState } from "react";
+import styles from "../style/login.module.css";
+
+export default function Login() {
+  const [showPassword, setShowPassword] = useState(false);
+  const [email, setEmail] = useState("");
+  const [senha, setSenha] = useState("");
+
+  function togglePassword() {
+    setShowPassword((prev) => !prev);
+  }
+
+  function clickEntrar(e) {
+    e.preventDefault();
+
+    const json = { email, senha };
+    const informacoes_login = JSON.stringify(json);
+    console.log(informacoes_login);
+
+    // Aqui você pode implementar a lógica para enviar os dados ao back-end
+  }
+
+  return (
+    <>
+      <div className={styles.container}>
+        <h2 className={styles.title}>Login</h2>
+
+        <form className={styles.form} method="post" onSubmit={clickEntrar}>
+          <label className={styles.label} htmlFor="email_usuario">
+            Usuário
+          </label>
+          <input
+            className={styles.input}
+            type="text"
+            name="email_usuario"
+            id="email_usuario"
+            required
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+
+          <label className={styles.label} htmlFor="senha_usuario">
+            Senha
+          </label>
+
+          <div className={styles.inputContainer}>
+            <input
+              className={`${styles.input} ${styles.inputContainerInput}`}
+              type={showPassword ? "text" : "password"}
+              name="senha_usuario"
+              id="senha_usuario"
+              minLength={8}
+              required
+              value={senha}
+              onChange={(e) => setSenha(e.target.value)}
+            />
+            <img
+              className={styles.iconEye}
+              onClick={togglePassword}
+              alt={showPassword ? "Esconder senha" : "Mostrar senha"}
+            />
+          </div>
+
+          <a href="#" className={styles.link}>
+            Esqueci a senha
+          </a>
+
+          <button type="button" className={styles.loginGoogle}></button>
+
+          <button type="submit" className={styles.entrarLogin}>
+            Entrar
+          </button>
+        </form>
+      </div>
+    </>
+  );
+}
